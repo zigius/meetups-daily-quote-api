1abd68d928abc90b5867752614357ea296c49ba6
Daniel Singer <zigius@gmail.com>
2019-06-19 16:32:19 +0300

    hey, look over there!

--- src/clients/dailyQuoteClient.js
+++ src/clients/dailyQuoteClient.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const request     = require('superagent');
+const request = require('superagent');
 
 exports.getDailyQuote = async () => {
 
--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 'use strict';
 
-const Koa        = require('koa');
-const Router     = require('koa-router');
-const config     = require('config');
-const bodyParser = require('koa-bodyparser');
-const sqs        = require('./clients/sqs');
+const Koa              = require('koa');
+const Router           = require('koa-router');
+const config           = require('config');
+const bodyParser       = require('koa-bodyparser');
+const sqs              = require('./clients/sqs');
+const dailyQuoteClient = require('./clients/dailyQuoteClient');
 
 const app    = new Koa();
 const router = new Router();
 
-router.post('/send', async (ctx, next) => {
+router.post('/send', async (ctx) => {
+
+    const to         = ctx.request.body.to;
+    const dailyQuote = await dailyQuoteClient.getDailyQuote();
     await sqs.sendMessage('SEND_DAILY_QUOTE', {
-        to        : ctx.request.body.to,
-        dailyQuote: ctx.request.body.dailyQuote
+        to,
+        dailyQuote
     });
 });
 
