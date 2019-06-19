db4b59c3103b0c92cd2b8d00a7233ad66e209552
Daniel Singer <zigius@gmail.com>
2019-06-19 17:14:56 +0300

    a few bits tried to escape, but we caught them

--- src/clients/dailyQuoteClient.js
+++ src/clients/dailyQuoteClient.js
@@ -3,7 +3,11 @@
 const request = require('superagent');
 
 exports.getDailyQuote = async () => {
-
-    const response = await request.get('https://quotes.rest/qod');
-    return response.body.contents.quotes[0].quote;
+    try {
+        const response = await request.get('https://quotes.rest/qod');
+        return `${response.body.contents.quotes[0].quote}. ${response.body.contents.quotes[0].author}`;
+    } catch (err) {
+        console.error(err);
+        return 'A man must pay to get a daily quote or else he will be throttled. Zigius';
+    }
 };
