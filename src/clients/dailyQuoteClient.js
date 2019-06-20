92cee9d25ffe040544a1131dad4bda5269d8dac9
Rami Moshe <rms.rami.m@gmail.com>
2019-06-20 16:28:12 +0300

    master-SHIT ===> GOLD

--- src/clients/dailyQuoteClient.js
+++ src/clients/dailyQuoteClient.js
@@ -2,12 +2,15 @@
 
 const request = require('superagent');
 
+
+const defaultQuote = 'A man must pay to get a daily quote or else he will be throttled. Zigius';
+
 exports.getDailyQuote = async () => {
     try {
         const response = await request.get('https://quotes.rest/qod');
         return `${response.body.contents.quotes[0].quote}. ${response.body.contents.quotes[0].author}`;
     } catch (err) {
         console.error(err);
-        return 'A man must pay to get a daily quote or else he will be throttled. Zigius';
+        return defaultQuote;
     }
 };
